7c3c6867a2a86f5d3299a05e026aa4fe15308fca
katyaputintseva <katya.putintseva@gmail.com>
2018-05-29 17:19:22 +0200

    bug fix

--- classes.py
+++ classes.py
@@ -107,6 +107,7 @@ class Read:
 
         self.n_mutations = len(self.mutations)
         self.mutations_pos = [int(x[1:-1]) for x in self.mutations]
+        self.mutation_type = [x[0]+x[-1] for x in self.mutations]
 
 
 
